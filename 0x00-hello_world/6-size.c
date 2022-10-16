c94a46578ef00c74263bc9bc927f8ce207c22201
lylajoy <ojijoyseun@gmail.com>
2022-10-16 14:07:06 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int integerType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	/**
+	 *sizeof operator is used to evaluate the size of a variable
+ 	 *
+	 * Return: 0
+	 */
+	printf("Size of int: %ld bytes\n" ,sizeof(integerType));
+	printf("Size of float: %ld bytes\n" ,sizeof(floatType));
+  	printf("Size of double: %ld bytes\n" ,sizeof(doubleType));
+	printf("Size of char: %ld bytes\n" ,sizeof(charType));
+
+	return 0;
+}
+
