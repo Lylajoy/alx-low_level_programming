8de2d7091d6de9270792bf1e418f0c3cbb7c112b
lylajoy <ojijoyseun@gmail.com>
2022-10-16 13:28:48 -0700

    hello puts and print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+
+ * puts - writes the string to the output stream
+
+ */
 
 int main(void)
 {	
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+
+ * printf sends formatted output to screen
+
+ */
 
 int main(void)
 {
