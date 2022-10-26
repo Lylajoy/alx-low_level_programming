28edbc9a4b2c6448e113610168cb52f9e7412c68
lylajoy <ojijoyseun@gmail.com>
2022-10-25 18:29:44 -0700

    change_to_98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,25 @@
+#ifndef _MAIN_H
+#define _ MAIN_H	
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+
+ * reset_to_98 - resets value of pointer to 98
+
+ * @n: pointer to reset to 98
+
+ *
+
+ * Return: void
+
+ */
+
+void reset_to_98(int *n)
+
+{
+
+	*n = 98;
+
+}
+#endif /*_MAIN_H*/
