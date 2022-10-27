6a3a24d24a4ed9686f7019e9e66bf035b03cb3b1
lylajoy <ojijoyseun@gmail.com>
2022-10-27 04:16:33 -0700

    print letters with omission

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-	int ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	putchar(ch);
-	ch != 'e';
-	ch != 'q';
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		putchar(letter);
+	}
 
 	putchar('\n');
 	return (0);
