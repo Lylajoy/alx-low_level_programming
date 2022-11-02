86ecb02f4816e827bf51b5e440137005f7690690
lylajoy <ojijoyseun@gmail.com>
2022-11-02 14:37:46 -0700

    print in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,17 +3,11 @@
 
 
 /**
-
  * _print_rev_recursion - Prints a string in reverse
-
  * @s: string
-
  *
-
  * Return: On success 1.
-
  * On error, -1 is returned, and errno is set appropriately.
-
  */
 
 void _print_rev_recursion(char *s)
