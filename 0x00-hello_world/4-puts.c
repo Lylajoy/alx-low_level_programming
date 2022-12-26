a8962b11079b6645935c43a93de542765d7f5fac
lylajoy <ojijoyseun@gmail.com>
2022-12-26 11:32:20 -0800

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+
+
 /**
 
- * puts - writes the string to the output stream
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
 
  */
 
 int main(void)
-{	
-	 puts("\"Programming is like building a multilingual puzzle\n" );
-	 return (0);
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
